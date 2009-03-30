344e36043bcb0b6bd0c3e443c2ce0213c68a09a2
seb <seb@00e1ce14-eaab-488a-b636-4dc9bedd6b81>
2009-03-30 15:23:46 +0000

    Updated svn version

--- qtesterman/Base.py
+++ qtesterman/Base.py
@@ -33,12 +33,12 @@ import Resources
 
 # Versioning scheme:
 # A.B.C for 'releasable' versions. 
-# A.B.C-svn for work in progress versions that will eventually 
-# be released as A.B.C
+# A.B.C-svn[-YYYYMMDD] for work in progress versions that will eventually 
+# be released as A.B.C+1 or A.B+1.0
 # A += 1: major design changes. 
 # B += 1: new significant features added
 # C += 1: bugfixes and/or minor features added
-CLIENT_VERSION = "1.1.1-svn"
+CLIENT_VERSION = "1.1.96-svn-20090330"
 CLIENT_NAME = "QTesterman"
 
 def getClientVersion():
