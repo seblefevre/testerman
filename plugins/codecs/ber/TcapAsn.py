7d93e8866e37c98520601cfb6169bf8abbb2b0e4
seb <seb@00e1ce14-eaab-488a-b636-4dc9bedd6b81>
2010-09-09 13:16:56 +0000

    Fixed TCAP ASN.1: now uses operationCode instead of opCode, and the correct ERROR choice in returnError components, as specified in Q.733.

--- plugins/codecs/ber/TcapAsn.py
+++ plugins/codecs/ber/TcapAsn.py
@@ -1,4 +1,4 @@
-# Auto-generated at Mon, 30 Mar 2009 14:43:03 +0000
+# Auto-generated at Thu, 09 Sep 2010 13:15:00 +0000
 # with the following command line:
 # ./py_output.py --implicit asn/tcap.asn
 import Yapasn1 as asn1
@@ -9,10 +9,8 @@ ERROR=asn1.CHOICE ([('localValue',None,asn1.INTEGER_class ([],None,None)),
     ('globalValue',None,asn1.OBJECT_IDENTIFIER)])
 ReturnResultProblem=asn1.INTEGER_class ([('unrecognizedInvokeID',0),('returnResultUnexpected',1),('mistypedParameter',2)],None,None)
 Parameter=asn1.ANY
-ErrorCode=asn1.CHOICE ([('nationaler',None,asn1.TYPE(asn1.IMPLICIT(19,cls=asn1.PRIVATE_FLAG),asn1.INTEGER_class ([],-32768,32767))),
-    ('privateer',None,asn1.TYPE(asn1.IMPLICIT(20,cls=asn1.PRIVATE_FLAG),asn1.INTEGER_class ([],None,None)))])
 ReturnError=asn1.SEQUENCE ([('invokeID',None,InvokeIdType,0,None),
-    ('errorCode',None,ErrorCode,0,None),
+    ('errorCode',None,ERROR,0,None),
     ('parameter',None,Parameter,1,None)], seq_name = 'ReturnError')
 ReturnErrorProblem=asn1.INTEGER_class ([('unrecognizedInvokeID',0),('returnErrorUnexpected',1),('unrecognizedError',2),('unexpectedError',3),('mistypedParameter',4)],None,None)
 InvokeProblem=asn1.INTEGER_class ([('duplicateInvokeID',0),('unrecognizedOperation',1),('mistypedParameter',2),('resourceLimitation',3),('initiatingRelease',4),('unrecognizedLinkedID',5),('linkedResponseUnexpected',6),('unexpectedLinkedOperation',7)],None,None)
@@ -25,7 +23,7 @@ ExternalPDU=asn1.TYPE(asn1.IMPLICIT(8,cls=asn1.UNIVERSAL_FLAG),asn1.SEQUENCE ([(
 OrigTransactionID=asn1.TYPE(asn1.IMPLICIT(8,cls=asn1.APPLICATION_FLAG),asn1.OCTSTRING)
 DestTransactionID=asn1.TYPE(asn1.IMPLICIT(9,cls=asn1.APPLICATION_FLAG),asn1.OCTSTRING)
 DialoguePortion=asn1.TYPE(asn1.EXPLICIT(11,cls=asn1.APPLICATION_FLAG),asn1.EXTERNAL)
-Reject=asn1.SEQUENCE ([('invokeIDRej',None,    asn1.CHOICE ([('derivable',None,InvokeIdType),
+Reject=asn1.SEQUENCE ([('invokeID',None,    asn1.CHOICE ([('derivable',None,InvokeIdType),
         ('not_derivable',None,asn1.NULL)]),0,None),
     ('problem',None,    asn1.CHOICE ([('generalProblem',None,asn1.TYPE(asn1.IMPLICIT(0,cls=asn1.CONTEXT_FLAG),GeneralProblem)),
         ('invokeProblem',None,asn1.TYPE(asn1.IMPLICIT(1,cls=asn1.CONTEXT_FLAG),InvokeProblem)),
@@ -33,10 +31,10 @@ Reject=asn1.SEQUENCE ([('invokeIDRej',None,    asn1.CHOICE ([('derivable',None,I
         ('returnErrorProblem',None,asn1.TYPE(asn1.IMPLICIT(3,cls=asn1.CONTEXT_FLAG),ReturnErrorProblem))]),0,None)], seq_name = 'Reject')
 Invoke=asn1.SEQUENCE ([('invokeID',None,InvokeIdType,0,None),
     ('linkedID',None,asn1.TYPE(asn1.IMPLICIT(0,cls=asn1.CONTEXT_FLAG),InvokeIdType),1,None),
-    ('opCode',None,OPERATION,0,None),
+    ('operationCode',None,OPERATION,0,None),
     ('parameter',None,Parameter,1,None)], seq_name = 'Invoke')
 ReturnResult=asn1.SEQUENCE ([('invokeID',None,InvokeIdType,0,None),
-    ('resultretres',None,    asn1.SEQUENCE ([('opCode',None,OPERATION,0,None),
+    ('resultretres',None,    asn1.SEQUENCE ([('operationCode',None,OPERATION,0,None),
         ('parameter',None,Parameter,1,None)], seq_name = None),1,None)], seq_name = 'ReturnResult')
 Component=asn1.CHOICE ([('invoke',None,asn1.TYPE(asn1.IMPLICIT(1,cls=asn1.CONTEXT_FLAG),Invoke)),
     ('returnResultLast',None,asn1.TYPE(asn1.IMPLICIT(2,cls=asn1.CONTEXT_FLAG),ReturnResult)),
